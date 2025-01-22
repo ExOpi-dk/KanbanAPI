ad1abacbcd1148d43bd206f7f11a4f2b0df35cb0
Jonathan Thomassen <jt@exopi.dk>
2025-01-22 11:40:52 +0100

    Overrides SaveChangesAsync method to automatically update values for Created and LastUpdated

--- Kanban/Migrations/20250122103925_Kanban008.Designer.cs
+++ Kanban/Migrations/20250122103925_Kanban008.Designer.cs
@@ -4,6 +4,7 @@ using Kanban.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kanban.Migrations
 {
     [DbContext(typeof(KanbanContext))]
-    partial class KanbanContextModelSnapshot : ModelSnapshot
+    [Migration("20250122103925_Kanban008")]
+    partial class Kanban008
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
